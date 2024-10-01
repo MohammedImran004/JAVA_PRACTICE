885ff18180664b5d6b9af5b40a67142ef55ddda9
MohammedImran004 <143481755+MohammedImran004@users.noreply.github.com>
2024-10-01 08:52:18 +0530

    STARTED OOPS CONCEPT

---
+++ OOPS_CONCPET/ArrayList.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.util.ArrayList;
+public class ArrayList{
+    public static void even(ArrayList<Integer> n){
+        System.out.println("List of Even Numbers:");
+        System.out.println(n);
+    }
+    public static void mul(ArrayList<Integer> n){
+        System.out.println("List of Even Numbers multiplied by 2:");
+        for(int i=0;i<n.size();i++)
+        {
+            n.set(i,n.get(i)*2);
+        }
+        System.out.println(n);
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        ArrayList<Integer> al = new ArrayList<Integer>(N);
+        for(int i=2;i<=N;i+=2)
+        {
+                al.add(i);
+        }
+        even(al);
+        mul(al);
+    }
+}
